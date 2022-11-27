5bb6ba0dc9fbe865e8e5195fa50ac727a377588a
Alesya <alesyamurales@gmail.com>
2022-11-27 11:46:17 +0500

    Lesson7

---
+++ lesson7/src/redux/reducer/messageReducer.js
@@ -0,0 +1,20 @@
+
+
+const initialState = {
+    messages: [
+        {
+            id: 1,
+            title: 'Who you'
+        }
+    ]
+}
+
+export const messagesReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+
+
+        default:
+            return state
+    }
+}
